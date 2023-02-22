0ee2bf1673974169403c2881e94d4a20d37158f5
GaneshsaiBese <93912528+GaneshsaiBese@users.noreply.github.com>
2023-02-22 22:30:16 +0530

    Add files via upload

---
+++ expy.py
@@ -0,0 +1,20 @@
+import turtle
+t=turtle.Turtle()
+for i in range(100,110):
+    t.circle(i)
+t.penup()
+t.fd(100)
+t.pendown()
+for i in range(100,110):
+    t.circle(i)
+t.penup()
+t.lt(90)
+t.fd(30)
+t.lt(90)
+t.fd(50)
+t.pendown()
+for i in range(100,110):
+    t.circle(i)
+
+
+
